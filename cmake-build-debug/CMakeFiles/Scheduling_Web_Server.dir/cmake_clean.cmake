577f9eb23f60cef9a2c558c80777276710f8206e
Bruce <brucewmacdonald@gmail.com>
2017-06-30 00:19:53 -0300

    add clion project structure

---
+++ cmake-build-debug/CMakeFiles/Scheduling_Web_Server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scheduling_Web_Server.dir/network.c.o"
+  "CMakeFiles/Scheduling_Web_Server.dir/sws.c.o"
+  "Scheduling_Web_Server.pdb"
+  "Scheduling_Web_Server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Scheduling_Web_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
